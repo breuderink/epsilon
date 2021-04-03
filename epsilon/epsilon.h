7452b23b6073ce6339213a8447dc68c77b3d4d33
Boris Reuderink <boris@cortext.nl>
2021-04-03 17:01:51 +0200

    Add SORF code from gist.
    See https://gist.github.com/breuderink/13a13044a001a846efdd71a28291cdcf.

--- epsilon/epsilon.h
+++ epsilon/epsilon.h
@@ -1,3 +1,8 @@
 #include <stdint.h>
+#include <stddef.h>
 
 uint32_t xorshift32(uint32_t y);
+
+void fx_sorf(float *x, uint8_t nbits);
+void fx_fwht(float *x, uint8_t nbits);
+void fx_repeat(float *x1, size_t n1, float *x2, size_t n2);
