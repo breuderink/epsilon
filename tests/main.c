52452102f0728595c51bb741fca5b1ea2abeff53
Boris Reuderink <boris@cortext.nl>
2021-04-05 14:41:03 +0200

    Re-enable SORF tests.

--- tests/main.c
+++ tests/main.c
@@ -3,9 +3,11 @@
 GREATEST_MAIN_DEFS();
 
 SUITE_EXTERN(RNG);
+SUITE_EXTERN(SORF_tests);
 
 int main(int argc, char **argv) {
 	GREATEST_MAIN_BEGIN();
 	RUN_SUITE(RNG);
+	RUN_SUITE(SORF_tests);
 	GREATEST_MAIN_END();
 }
