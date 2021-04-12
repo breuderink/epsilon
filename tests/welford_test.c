fa5691bfbfd12ef84c971b35cd7da3f6dd9632f1
Boris Reuderink <boris@cortext.nl>
2021-04-12 10:00:26 +0200

    Extend tests for edge cases.

--- tests/welford_test.c
+++ tests/welford_test.c
@@ -1,13 +1,26 @@
 #include <epsilon.h>
 #include <greatest.h>
+#include <math.h>
 
-TEST welford_edge_cases() {
+TEST welford_edge_cases(float mu, float sigma) {
 	online_stats_t s = {0};
+	ASSERTm("Mean before observations should be NaN!", isnan(mean(&s)));
+	ASSERTm("Variance before observations should be NaN!", isnan(var(&s)));
 
-	ASSERT_EQ_FMT(mean(&s), 0, "%f");
+	observe(&s, mu - sigma);
+	ASSERT_EQ_FMT(mean(&s), mu - sigma, "%f");
+	ASSERT_EQ_FMT(var(&s), 0, "%f");
+
+	observe(&s, mu + sigma);
+	ASSERT_EQ_FMT(mean(&s), mu, "%f");
+	ASSERT_EQ_FMT(var(&s), sigma * sigma, "%f");
+
 	PASS();
 }
 
 SUITE(Welford) {
-	RUN_TEST(welford_edge_cases);
+	RUN_TESTp(welford_edge_cases, 0, 1);
+	RUN_TESTp(welford_edge_cases, 1, 1);
+	RUN_TESTp(welford_edge_cases, -5, 2);
+	RUN_TESTp(welford_edge_cases, 1e4, 0.1);
 }
