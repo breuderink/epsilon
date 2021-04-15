fcf9f1e449e799ed3ca981b88dfe365fa47141c5
Boris Reuderink <boris@cortext.nl>
2021-04-15 09:20:57 +0200

    Add note on variant of Welfords method.

--- epsilon/welford.c
+++ epsilon/welford.c
@@ -11,6 +11,10 @@ void Welford_observe(Welfords_method_t *w, size_t n, float x) {
 	float delta_after = (x - w->mean);
 	w->squared_diff += delta_before * delta_after;
 	assert(isfinite(w->squared_diff));
+
+	// Note that we can track the population variance instead of the squared
+	// difference. That seems more symmetrical, and would prevent squared_diff
+	// from getting to big for a fixed-point implementation.
 }
 
 void observe(online_stats_t *s, float x) {
