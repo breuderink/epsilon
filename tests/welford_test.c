8694e2309d249f3a084d031aac7f621b0d958ee2
Boris Reuderink <boris@cortext.nl>
2021-04-15 09:08:28 +0200

    Add tests for sample variance.

--- tests/welford_test.c
+++ tests/welford_test.c
@@ -12,35 +12,49 @@ TEST welford_test_mean() {
 	PASS();
 }
 
-TEST welford_test_var100() {
+TEST welford_test_var() {
 	online_stats_t s = {0};
 
 	for (int i = 0; i < 10; ++i) {
 		observe(&s, i);
 	}
+	/*
+	>>> import numpy as np
+	>>> np.var(np.arange(10))
+	8.25
+	>>> np.var(np.arange(10), ddof=1)
+	9.166666666666666
+	*/
 	ASSERT_IN_RANGE(8.25, pvariance(&s), 1e-6);
+	ASSERT_IN_RANGE(9.1667, variance(&s), 1e-4);
 	PASS();
 }
 
 TEST welford_edge_cases(float mu, float sigma) {
 	online_stats_t s = {0};
-	ASSERTm("Mean before observations should be NaN!", isnan(mean(&s)));
-	ASSERTm("Variance before observations should be NaN!", isnan(pvariance(&s)));
+	ASSERTm("mean() before observations should be NaN!", isnan(mean(&s)));
+	ASSERTm("pvariance() before observations should be NaN!",
+	        isnan(pvariance(&s)));
+	ASSERTm("variance() before observations should be NaN!",
+	        isnan(variance(&s)));
 
 	observe(&s, mu - sigma);
 	ASSERT_EQ_FMT(mean(&s), mu - sigma, "%f");
 	ASSERT_EQ_FMT(pvariance(&s), 0.0, "%f");
+	ASSERTm("variance() after first observation should be NaN!",
+	        isnan(variance(&s)));
 
 	observe(&s, mu + sigma);
 	ASSERT_EQ_FMT(mean(&s), mu, "%f");
 	ASSERT_IN_RANGE(sigma * sigma, pvariance(&s), 1e-4);
+	ASSERT_IN_RANGE(sigma * sigma * (2/1), variance(&s), 1e-3);
 
 	PASS();
 }
 
 SUITE(Welford) {
 	RUN_TEST(welford_test_mean);
-	RUN_TEST(welford_test_var100);
+	RUN_TEST(welford_test_var);
 
 	RUN_TESTp(welford_edge_cases, 0, 1);
 	RUN_TESTp(welford_edge_cases, 1, 1);
