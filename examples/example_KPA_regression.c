48597b97dbd8fd830e654942af6e7addd4e91125
Boris Reuderink <boris@cortext.nl>
2021-04-16 15:35:00 +0200

    Chanke KPA regressione example to use Gaussian kernel and sin target.

--- examples/example_KPA_regression.c
+++ examples/example_KPA_regression.c
@@ -1,33 +1,48 @@
-#include <assert.h>
 #include "epsilon.h"
+#include <assert.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+// Define problem-specific input structure. Our input is a single position.
+typedef struct {
+	float position;
+} input_t;
 
 /* Define a problem-specific kernel. A kernel defines a dot-product between
 inputs, and should be adapted to the problem.
 
 [1] Schölkopf, Bernhard, Alexander J. Smola, and Francis Bach. Learning with
-	kernels: support vector machines, regularization, optimization, and beyond.
-	MIT press, 2002. */
+kernels: support vector machines, regularization, optimization, and beyond.
+MIT press, 2002. */
 
 #define BUDGET 64
-static struct { float position; } instances[BUDGET] = {0};
-static float kernel(size_t i, size_t j) {
+static input_t instances[BUDGET] = {0};
+static float linear_kernel(size_t i, size_t j) {
 	float dot = 1; // Use 1 to implicitly use a bias term.
 	dot += instances[i].position * instances[j].position;
-	return dot * dot;
+	return dot;
+}
+
+static float Gaussian_kernel(size_t a, size_t b) {
+	float k_aa = linear_kernel(a, a);
+	float k_ab = linear_kernel(a, b);
+	float k_bb = linear_kernel(b, b);
+
+	// (a - b)^T (a - b) = a^T a - 2 a^T b + b^T b.
+	float squared_dist = k_aa - 2 * k_ab + k_bb;
+	return expf(-squared_dist * 0.3);
 }
 
 int main() {
-	PA_t PA = {.C = INFINITY, .eps = 0.1};
+	PA_t PA = {.C = 1000, .eps = 0.1};
 
 	// Define regressor.
 	float alpha[BUDGET] = {0};
 	KP_t regressor = {
 	    .alpha = alpha,
 	    .num_alpha = BUDGET,
-	    .kernel = &kernel,
+	    .kernel = &Gaussian_kernel,
 	};
 
 	online_stats_t loss = {0};
@@ -44,7 +59,7 @@ int main() {
 		float prediction = KPA_regress(&regressor, PA, i, NAN);
 
 		// Define target and update model.
-		float target = input * input - 10;
+		float target = sinf(input);
 		BKPA_regress(&regressor, PA, i, target);
 
 		// Track loss.
