92d56dcdf8a29e1e81137b0f3edd5d6c74be3cd1
Boris Reuderink <boris@cortext.nl>
2021-04-21 09:30:32 +0200

    Tweak FNV hash example.

--- examples/example_FNV_hash.c
+++ examples/example_FNV_hash.c
@@ -8,9 +8,9 @@ int main() {
     // fixed value. It should be fast to compute, and it should mimise
     // collisions, where different inputs result in the same output hash value.
 
-    // Compute the hash of two similar strings. Their hashes should be
+    // Compute the hash of similar strings. Their hashes should be
     // different.
-    const char *data[] = {"Epsilon", "epsilon", "epsilon."};
+    const char *data[] = {"Foo", "foo", "foo."};
     for (int s = 0; s < sizeof(data) / sizeof(data[0]); ++s) {
         // Set string.
         const char *str = data[s];
@@ -18,8 +18,7 @@ int main() {
         // Compute hash.
         uint32_t hash = FNV1a_hash(str, strlen(str));
 
-        // Display results. Compare with results on
-        // https://sha256calc.com/hash/fnv1a32.
+        // Display results. 
         printf("Hashed '%s' to %#010x.\n", str, hash);
     }
     return 0;
